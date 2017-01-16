3d00c4a3c3d91044b0e6caacc5f6ca6463644620
swzs <204173334@qq.com>
2017-01-16 17:09:10 +0800

    修改路由设置!

--- web/pay_api/__init__.py
+++ web/pay_api/__init__.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from . import wx_demo, public_number
-from web.pay_api.wx_public import wx_client_user, wxpay_test
+from . import wx_public
 
 __author__ = 'raymondlei'
--- web/pay_api/wx_public/__init__.py
+++ web/pay_api/wx_public/__init__.py
@@ -4,4 +4,6 @@
 微信公众号相关接口
 """
 
+from . import wxpay_test
+
 __author__ = 'raymondlei'
